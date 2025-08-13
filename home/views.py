49cf053e875d8b570afbdbab027565c79f635e35
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-08-13 17:44:37 +0530

    31527

--- home/views.py
+++ home/views.py
@@ -7,3 +7,14 @@ def home(request):
     return render(request,'index.html',{'restaurant_name':restaurant.name if restaurant else "our Restaurant"})
 def about(request):
     return render(request,'about.html')
+
+def menu_list(request):
+    menu_items=[
+        {'name':'Pizza','price':80},
+        {'name':'Biryani','price':200},
+        {'name':'Meals','price':100},
+    ]
+    context={
+        'menu_list'=menu_items
+    }
+    return render(request,context)
