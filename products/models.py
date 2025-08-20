9814704de235ce35bf7c34c7cf25452d11b2be4c
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-08-20 23:46:51 +0530

    35571

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
 class Item(models.Model):
@@ -6,5 +7,18 @@ class Item(models.Model):
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
         return str(self.item_name)
+class Order(models.Model):
+    STATUS_CHOICES=[
+        ('PENDING','pending'),
+        ('CONFIRMED','confirmed'),
+        ('DELIVERED','delivered'),
+        ('CANCELLED','cancelled'),
+    ]
+    customer=models.Foreignkey(User,on_delete=models.CASCADE,related_name='orders')
+    order_items=models.ManyToManyField(Item)
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2)
+    status=models.CharField(max_length=20,choices=STATUS_CHOICES,default='PENDING')
+    created_at=models.DateTimeField(auto_now_add=True)
