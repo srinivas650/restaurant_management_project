0f724170d8f961eb85ae403df465053e27f04a78
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-08-21 00:05:37 +0530

    35568

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+class UserProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=150)
+    email=models.EmailField(unique=True)
+    phone_number=models.CharField(max_length=15,blank=True,null=True)
