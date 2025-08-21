02cb926d43418d8864a3927a1a34c99ce1202fce
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-08-21 23:46:44 +0530

    36170

--- home/models.py
+++ home/models.py
@@ -10,4 +10,8 @@ class Feedback(models.Model):
     comments=models.TextField()
     submitted_at=models.DateTimeField(auto_now_add=True)
     
-class
+class Contact(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    
