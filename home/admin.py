25f590710c9b7ca8434e019fd4e105b042597933
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-08-28 17:54:12 +0530

    39396

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Restaurantinfo
+from . models import RestaurantInfo
 
 # Register your models here.
-admin
+admin.site.register(RestaurantInfo)
