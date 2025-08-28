817dc18bf198fde4b2316fdedd320e716f4fb408
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-08-28 17:53:33 +0530

    39396

--- home/models.py
+++ home/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class RestaurantInfo(models.Model):
     name=models.CharField(max_length=255)
-    def __str__(self):
-        return self.name
+    street=models.CharField(max_length=200)
+    city=models.CharField(max_length=100)
+    state=models.CharField(max_length=100)
+    zipcode=models.CharField(max_length=20)
 
 class Feedback(models.Model):
     comments=models.TextField()
