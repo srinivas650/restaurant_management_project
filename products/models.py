ed39e629f3cee98520d81088efb28c24a1a3a278
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-01 22:14:35 +0530

    40598

--- products/models.py
+++ products/models.py
@@ -24,3 +24,8 @@ class Order(models.Model):
     total_amount=models.DecimalField(max_digits=10,decimal_places=2)
     status=models.CharField(max_length=20,choices=STATUS_CHOICES,default='PENDING')
     created_at=models.DateTimeField(auto_now_add=True)
+def CartItem(models.Model):
+    user=models.Foreignkey(User,on_delete=models.CASCADE)
+    item=models.Foreignkey(Item,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    
