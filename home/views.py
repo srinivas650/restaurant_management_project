ed70d93f9cc4b3f70ac58c1f67695f9e3be01a7e
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-01 19:32:18 +0530

    40287

--- home/views.py
+++ home/views.py
@@ -42,14 +42,7 @@ def contact(request):
     if request.method=='POST':
         form=ContactForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('contact')
-    else:
-        form=ContactForm()
-    return render(request,'contact.html',{'form':form})
-
-    }
-    return render(request,'contact.html',contact_info)
+            name=form.cleaned_data['name']
 def index(request):
     restaurant=RestaurantInfo.objects.first()
     context={
