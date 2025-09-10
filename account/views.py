5f705a195278d5774cc5db023c88db0746ebf1ad
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-10 19:49:34 +0530

    43515

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+
 
 # Create your views here.
+
+def home2(request):
+    if request.method=="POST":
+        data=request.POST
+        username=data.get('username')
+        password=data.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user in not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'invalid credentials')
+    return render(request,'account.html')
