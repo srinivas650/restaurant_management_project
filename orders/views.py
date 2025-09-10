5e7645c5a2773ff71b2caed9540255b487a8acc1
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-10 18:49:59 +0530

    43516

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def order_confirmation(request):
+    order_number='ORD123456'
+    context={
+        'order_number':order_number
+    }
+    return render(request,'order_confirmation.html',context)
+    
