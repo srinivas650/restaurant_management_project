c63368ad219e292024d0ba9bd35bfeec0357aa05
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-17 23:42:43 +0530

    45457

--- orders/models.py
+++ orders/models.py
@@ -5,3 +5,10 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
+class Coupon(models.Model):
+    code=models.CharField(max_length=20,umique=True)
+    idscount_percent=models.IntegerField(default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.code
