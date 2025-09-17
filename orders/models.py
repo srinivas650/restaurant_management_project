1c2d9b45667bcb5fff632fc26cb0c0acaa219b7b
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-17 23:47:51 +0530

    45455

--- orders/models.py
+++ orders/models.py
@@ -12,3 +12,11 @@ class Coupon(models.Model):
 
     def __str__(self):
         return self.code
+
+class Order(models.Model):
+    customer_name=models.CharField(max_length=255)
+    order_date=models.DateTimeField(auto_now_add=True)
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2)
+    status=models.ForiegnKey(OrderStatus,on_delete=models.SET_NULL,null=True)
+    def __str__(self):
+        return f'order #{self.id}-{self.customer_name}'
