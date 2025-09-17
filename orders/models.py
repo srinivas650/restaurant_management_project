285989b128557c272f6fc54e9b808c09dde4d0c8
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-17 22:43:24 +0530

    45456

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-# Create your models here.
-from .models import Order
+class OrderStatus(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.name
