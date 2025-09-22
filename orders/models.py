16443dc4f2f75589862ec63e30ce93d366ecf5b2
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-22 21:08:44 +0530

    49629

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class OrderStatus(models.Model):
     name=models.CharField(max_length=50,unique=True)
@@ -14,9 +15,19 @@ class Coupon(models.Model):
         return self.code
 
 class Order(models.Model):
+    user=models.ForiegnKey(User,on_delete=models.CASCADE)
     customer_name=models.CharField(max_length=255)
     order_date=models.DateTimeField(auto_now_add=True)
     total_amount=models.DecimalField(max_digits=10,decimal_places=2)
     status=models.ForiegnKey(OrderStatus,on_delete=models.SET_NULL,null=True)
     def __str__(self):
         return f'order #{self.id}-{self.customer_name}'
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.CASCADE,related_name="items")
+    product_name=models.CharField(max_length=200)
+    quantity=models.PositiveIntegerField()
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product_name}(x{self.quantity})"
+        
