e63fc21502e7c1df11a9d388ef95f827410a80e2
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-23 23:16:07 +0530

    49628

--- products/models.py
+++ products/models.py
@@ -28,4 +28,21 @@ def CartItem(models.Model):
     user=models.Foreignkey(User,on_delete=models.CASCADE)
     item=models.Foreignkey(Item,on_delete=models.CASCADE)
     quantity=models.PositiveIntegerField(default=1)
+
+class MenuCategory(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItems(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
+    category=models.Foreignkey(MenuCategory,on_delete=models.CASCADE,related_name='items')
+    
+    def __str__(self):
+        return self.name
+
+
+
     
