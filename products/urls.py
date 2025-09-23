6c83f86f4e67fdc5603f31430bde13ef3ba167c1
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-24 00:30:53 +0530

    49626

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+from . views import MenuItemsViewSet
+
+router=DefaultRouter()
+router.register(r'items',MenuItemsViewSet,basename='menu-items')
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/',restaurant_menu,name='restaurant-menu'),
     path('menu/',views.Menu_view,name='menu'),
     path('items-by-category',MenuItemsByCategory.as_view(),name='items-by-category'),
+    path("",include(router.urls)),
+
 ]
