4c1603abcee9218d99bda3c9b776b83524706b61
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-23 23:18:09 +0530

    49628

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item,MenuItems
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+class MenuItemSerializer(serializers.ModelSerializers):
+    class Meta:
+        model=MenuItems
+        fields=['id','name','price','category']
