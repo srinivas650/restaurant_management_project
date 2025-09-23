e872c820b7d0e66341aaf4e5ce10c52dacaaf0f2
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-23 23:28:54 +0530

    49628

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/',restaurant_menu,name='restaurant-menu'),
     path('menu/',views.Menu_view,name='menu'),
+    path('items-by-category',MenuItemsByCategory.as_view(),name='items-by-category'),
 ]
