69048065f2550abd8fe9ec1b6c23a1c222749529
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-25 23:24:11 +0530

    50450

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('order',views.order_confirmation,name='orderconfirmation'),
     path('history',OrderHistoryView.as_view(),name='order-history'),
+    path('<int:id>/',orderDetailView.as_view(),name='order-detail'),
 ]
