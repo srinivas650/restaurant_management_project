af8b9c6a3e774cc88b3317c183edb046b23325b9
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-09-25 23:21:17 +0530

    50450

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from . models import Order,OrderItem
-
+from home.models import MenuItem
 class OrderItemSerializer(serializers.ModelSerializer):
+    items=MenuItemSerializer(many=True,read_only=True)
+    customer=serializers.StringRelatedField()
     class Meta:
         model=OrderItem
         fields=['product_name','quantity'.'price']
@@ -11,4 +13,9 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model=Order
         fields=['id','order_date','total_price','items']
-        
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MenuItem
+        fields=['id','name','price']
+
