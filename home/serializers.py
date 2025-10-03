8f41598462e9c99c9082b8660cd290f01602ff41
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-10-03 22:56:38 +0530

    52746

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from . models import RestaurantInfo
+from . models import RestaurantInfo,Table
 class RestaurantInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model=RestaurantInfo
         fields=['name']
-        
+    
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Table
+        fields=['table_number','capacity','is_available']
+
