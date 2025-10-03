42d434fcc40731f55b8347943e3d6efb3e72ddec
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-10-03 22:53:34 +0530

    52745

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('contact/',views.contact,name='contact'),
     path('faq/',views.faq_view,name="faq"),
     path('api/restaurant/'RestaurantListAPIView.as_view(),name='restaurant-list'),
+    path('api/tables/',TableListAPIView.as_view(),name='table-list'),
+    path('api/tables/<int:pk>/',TableDetailAPIView.as_view(),name='table-detail'),
 ]
