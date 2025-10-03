be3b540e5500f3374a0a492c926b1f593576e271
srinivas650 <119308885+srinivas650@users.noreply.github.com>
2025-10-03 22:58:17 +0530

    52746

--- home/views.py
+++ home/views.py
@@ -111,4 +111,9 @@ class TableListAPIView(generics.ListAPIView):
 class TableDetailAPIView(generics.RetrieveAPIView):
     queryset=Table.objects.all()
     serializer_class=TableSerializer
+
+class AvailableTablesAPIView(generics.ListAPIView):
+    serializer_class=TableSerializer
+    def get_queryset(self):
+        return Table.objects.filter(is_available=True)
     
